9cb959c170edfe7ce8c529264bf9dbc6123b3237
Rob Christian <rm.rf.etc@gmail.com>
2016-10-17 00:45:03 -0700

    Include browser adaptations.

---
+++ runway-browser.js
@@ -0,0 +1,67 @@
+
+/*
+
+Semi-colon line terminators are just FUD. If your minifier can't handle this code, switch to one that is JS-compliant.
+http://blog.izs.me/post/2353458699/an-open-letter-to-javascript-leaders-regarding
+http://inimino.org/~inimino/blog/javascript_semicolons
+
+The only time you EVER need a semi-colon for statement termination:
+;[1,2,3].map(function(val){ 'do stuff' })
+;(function(){ 'do stuff' })
+
+*/
+
+var runway = require('./runway.js')
+var active_controller
+runway.bind('route added',function(){
+	var ctrl = runway.go(location.pathname)
+	if (ctrl && ctrl !== active_controller) {
+		active_controller = ctrl
+		active_controller()
+	}
+})
+
+module.exports = runway
+
+
+
+var onclick_els = ['A','BUTTON']
+
+document.onclick = function(event) {
+	event = event || window.event // IE specials
+	var target = event.target || event.srcElement // IE specials
+
+	if (onclick_els.indexOf(target.tagName) !== -1 && target.href) {
+		event.preventDefault()
+		processLink(target.href, target.dataset.ajax)
+	}
+}
+
+function processLink(href, ajax){
+	console.log('processLink', href)
+	href = href.replace(location.origin,'')
+	if (ajax !== 'none') {
+		goForward(href)
+		doRoute(href)
+		return false
+	}
+	return true
+}
+
+function doRoute(href){
+	active_controller = runway.go(href)
+	if (active_controller) active_controller()
+}
+
+function goForward(url){
+	if (history.pushState) history.pushState({url:url}, null, url)
+	else location.assign(url)
+}
+
+window.onpopstate = function(event){ doRoute(event.state.url) }
+
+function init(){
+	history.replaceState( {url:location.pathname}, null, location.pathname )
+}
+
+window.addEventListener ? addEventListener('load', init, false) : window.attachEvent ? attachEvent('onload', init) : (onload = init)
